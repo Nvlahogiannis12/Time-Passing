f582b6ab24d765081f5c58af24753839a4d7a276
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-05-20 17:30:38 +0000

    Added Music

---
+++ script.js
@@ -0,0 +1,35 @@
+let count = 0;
+let countMinute = 0;
+let countHour = 0;
+let countDay = 0;
+
+window.onload = function(){
+    let audio = new Audio("audio/Jono- Please Hold.mp3")
+    audio.loop = true
+    audio.play()
+}
+const timer = setInterval(() => {
+  count++;
+  document.getElementById("counter").innerText = `Seconds passed: ${count}`;
+
+  if (count % 60 === 0) {
+    countMinute++;
+    document.getElementById("minuteCounter").innerText = `Minutes passed: ${countMinute}`;
+
+    if (countMinute % 60 === 0) {
+      countHour++;
+      countMinute = 0; // reset minutes after an hour
+      document.getElementById("hourCounter").innerText = `Hours passed: ${countHour}`;
+
+      if (countHour % 24 === 0){
+        countDay++;
+        countHour = 0;
+        document.getElementById("dayCounter").innerText = `Days passed: ${countDay}`;
+      }
+    }
+  }
+
+  if (count >= 9999999999) {
+    clearInterval(timer); 
+  }
+}, 1000);
